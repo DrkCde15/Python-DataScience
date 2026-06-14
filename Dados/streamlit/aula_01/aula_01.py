b4cf030626ce2ac1b73036358cef6624f53f521e
DrkCde15 <jcesarsantana215@gmail.com>
2026-06-14 15:23:12 -0300

    aula 01 de streamlit

---
+++ Dados/streamlit/aula_01/aula_01.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+# Título da aplicação
+st.title("Minha Primeira Aplicação Streamlit")
+
+# Cabeçalho
+st.header("Bem-vindo ao Streamlit!")
+
+# Subcabeçalho
+st.subheader("Vamos aprender a criar aplicações web interativas")
+
+# Subtítulo
+st.markdown("### O que é Streamlit?")
+
+# Texto generico
+st.write("""
+         Streamlit é uma biblioteca de código aberto em Python que permite 
+         criar aplicações web interativas de forma rápida e fácil. 
+         Com Streamlit, você pode transformar seus scripts Python em aplicativos web 
+         sem precisar de conhecimentos avançados de desenvolvimento web.""")
+
+# texto literal
+st.text("Vamos começar a explorar o Streamlit juntos!")
---
+++ Dados/streamlit/aula_01/exc_01.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("Meu perfil 👋")
+
+# Cabeçalho
+st.header("Bem-vindo ao site com Streamlit! ⭐")
+
+# Subcabeçalho
+st.subheader("Meu nome é Júlio César, sou um entusiasta de tecnologia e apaixonado por programação. 🚀")
+
+# Subtítulo
+st.markdown("""
+            **Formado em ADS** 💻
+            Trabalhei como **estagiário em Engenharia de Dados** 📊
+            As áreas que  gosto de estudar são:
+            * CyberSegurança;
+            * Análise/Engenharia de dados;
+            * Programação Web voltada para backend;
+            * Inteligencia Artificial e Automações.
+            """)
+
+st.write("""Espero que tenha gostado do meu perfil! 😊""")
